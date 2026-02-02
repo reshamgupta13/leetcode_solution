c78f54157c429e9aee2f65aa14ec6c1b5fff5d9b
Resham Gupta <reshamgupta2005@gmail.com>
2026-02-02 12:29:08 +0530

    Time: 1 ms (100%), Space: 42.1 MB (100%) - LeetHub

---
+++ 3827-count-monobit-integers/3827-count-monobit-integers.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countMonobit(int n) {
+        int count = 1;
+        int val = 1; 
+
+        while (val <= n) {
+            count++;
+            val = (val << 1) | 1; 
+        }
+
+        return count;
+    }
+}

202ed71e9869ba7a41ca84a651a2be076a34f380
Resham Gupta <reshamgupta2005@gmail.com>
2026-03-26 00:36:05 +0530

    Time: 64 ms (10.1%), Space: 84.6 MB (5.02%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+int solveRec(string s1, string s2, int m, int n){
+        if(m==0) return n;
+        if(n==0) return m;
+        if(s1[m-1]==s2[n-1]){
+            return solveRec(s1,s2,m-1,n-1);
+        }
+         int removeOp  = solveRec(s1, s2, m-1, n);
+        int insertOp  = solveRec(s1, s2, m, n-1); 
+        int replaceOp = solveRec(s1, s2, m-1, n-1); 
+        return 1 + min({removeOp, insertOp, replaceOp}); 
+    } 
+    int solveMem(string s1, string s2, int m, int n,vector<vector<int>> &dp){
+            if(m==0) return n;
+            if(n==0) return m;
+             if(dp[m][n] != -1) return dp[m][n];
+            if(s1[m-1]==s2[n-1]){
+                return dp[m][n]= solveMem(s1,s2,m-1,n-1,dp);
+            }
+            int removeOp  = solveMem(s1, s2, m-1, n,dp);
+            int insertOp  = solveMem(s1, s2, m, n-1,dp); 
+            int replaceOp = solveMem(s1, s2, m-1, n-1,dp); 
+            dp[m][n]=1 + min({removeOp, insertOp, replaceOp}); 
+            return dp[m][n];
+    }
+    int minDistance(string word1, string word2) {
+        int m=word1.size();
+        int n=word2.size();
+       //return solveRec(word1, word2, m, n);
+       vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
+       return solveMem(word1,word2,m,n,dp);
+        
+    }
+};

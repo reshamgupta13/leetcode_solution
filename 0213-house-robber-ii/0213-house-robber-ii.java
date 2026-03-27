da5b54040a99b6952f29c6f71739d055a6455591
Resham Gupta <reshamgupta2005@gmail.com>
2026-03-28 00:02:24 +0530

    Time: 0 ms (100%), Space: 42.6 MB (92.94%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    public int solve(int[] nums, int i, int end, int[] dp){
+        if(i > end) return 0;
+
+        if(dp[i] != -1) return dp[i];
+
+        int include = nums[i] + solve(nums, i + 2, end, dp);
+        int exclude = solve(nums, i + 1, end, dp);
+
+        return dp[i] = Math.max(include, exclude);
+    }
+
+    public int rob(int[] nums) {
+        int n = nums.length;
+
+        if(n == 1) return nums[0];
+
+        int[] dp1 = new int[n];
+        int[] dp2 = new int[n];
+        Arrays.fill(dp1, -1);
+        Arrays.fill(dp2, -1);
+
+        int case1 = solve(nums, 0, n - 2, dp1);
+        int case2 = solve(nums, 1, n - 1, dp2);
+
+        return Math.max(case1, case2);
+    }
+}

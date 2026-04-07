c3084555733a6a377354ae9f372a9b9b3893f9a2
Resham Gupta <reshamgupta2005@gmail.com>
2026-04-07 23:53:10 +0530

    Time: 0 ms (100%), Space: 43.2 MB (92.68%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -3,8 +3,8 @@ class Solution {
         int k=0;
         for(int i=0;i<nums.length; i++){
             if(nums[i]!=val){
-                nums[k]=nums[i];
-                k++;
+                nums[k++]=nums[i];
+                
             }
         }
         return k;

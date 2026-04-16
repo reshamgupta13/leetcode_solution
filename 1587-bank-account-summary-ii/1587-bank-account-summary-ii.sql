74d43d9c6beaeec942d3adb66dfdf8a57697c435
Resham Gupta <reshamgupta2005@gmail.com>
2026-04-16 23:32:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
+++ 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
@@ -1,9 +1,9 @@
 SELECT 
     u.name,
-    SUM(COALESCE(t.amount, 0)) AS balance
+    SUM(t.amount) AS balance
 FROM Users u
 LEFT JOIN Transactions t
     ON u.account = t.account
 GROUP BY u.name
-HAVING SUM(COALESCE(t.amount, 0)) > 10000
+having sum(amount)>10000
 ORDER BY u.name ASC;

7a6bd783a8d42ad36bd27aca232692e7371a8823
Resham Gupta <reshamgupta2005@gmail.com>
2026-05-27 23:47:45 +0530

    Time: 0 ms (100%), Space: 42.2 MB (78.23%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isHappy(int n) {
+        if(n == 1 || n == 7) return true;
+        else if(n < 10) return false;
+        int m = 0;
+        while(n != 0){
+            int tail = n % 10;
+            m += tail * tail;
+            n = n/10;
+        }
+        return isHappy(m);
+    }
+}

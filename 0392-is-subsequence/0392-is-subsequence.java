a326656657ca7ca85fe2fa85e97c1ed55c5fa683
Resham Gupta <reshamgupta2005@gmail.com>
2026-07-31 15:42:15 +0530

    Time: 0 ms (100%), Space: 42.9 MB (44.9%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -3,14 +3,15 @@ class Solution {
         if(s.isEmpty()){
             return true;
         }
-        int i=0;
-        int j=0;
-        while(i<s.length() && j<t.length()){
-            if(s.charAt(i)==t.charAt(j)){
+        char ch[]=s.toCharArray();
+        char ch1[]=t.toCharArray();
+        int i=0, j=0;
+        while(i<ch.length && j<ch1.length){
+            if(ch[i]==ch1[j]){
                 i++;
             }
             j++;
         }
-    return i==s.length(); 
+    return ch.length==i; 
     }
 }

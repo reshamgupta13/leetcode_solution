f5f4e5ca3cfb944e20c824450c8646e9048a4d01
Resham Gupta <reshamgupta2005@gmail.com>
2026-07-31 15:11:56 +0530

    Time: 2 ms (99.32%), Space: 44.3 MB (64.84%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,28 +1,28 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        if(s.isEmpty()){
-            return true;
-        }
-        int start=0;
-        int end=s.length()-1;
-       
-        while(start<=end){
-                char currS=s.charAt(start);
-                char currE=s.charAt(end);
-            if(!Character.isLetterOrDigit(currS)){
-                start++;
-            }
-            else if(!Character.isLetterOrDigit(currE)){
-                end--;
-            }
-            else{
-                if(Character.toLowerCase(currS)!=Character.toLowerCase(currE)){
-                  return false;
-                }
-                start++;
-                end--;
-            }
-        }
+       if(s.isEmpty()){
         return true;
+       }
+       int start=0;
+       int end=s.length()-1;
+       while(start<=end){
+        char currS=s.charAt(start);
+        char currE=s.charAt(end);
+        if(!Character.isLetterOrDigit(currS)){
+            start++;
+        }
+        else if(!Character.isLetterOrDigit(currE)){
+            end--;
+        }
+        else{
+            if(Character.toLowerCase(currS)!=Character.toLowerCase(currE)){
+                return false;
+            }
+        start++;
+        end--;
+        }
+       
+       }
+       return true;
     }
 }
